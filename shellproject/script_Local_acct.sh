3bf393733d3b657324f59f048a3790ea4e657824
etokpa <tedmond2jc@gmail.com>
2025-05-01 13:07:42 -0400

    initial code

---
+++ shellproject/script_Local_acct.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script creates an account on the local system.
+# You will be prompted for the account name and password.
+
+# Ask for the user name.
+read -p 'Enter the username to create: ' USER_NAME
+
+# Ask for the real name.
+read -p 'Enter the name of the person who this account is for: ' COMMENT
+
+# Ask for the password.
+read -p 'Enter the password to use for the account: ' PASSWORD
+
+# Create the user.
+useradd -c "${COMMENT}" -m ${USER_NAME}
+
+# Set the password for the user.
+# NOTE: You can also use the following command:
+#    echo "${USER_NAME}:${PASSWORD}" | chpasswd
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Force password change on first login.
+passwd -e ${USER_NAME}
+
+
+0 8 * * 1 ./path/
+15 * * * ./path
+* * 1 * * ./path
+0 18 * * *
+0 5,17 * * * ./path
+0 0 * * 6,7 ./Path
+15 * * * 1,2,3,4,5,6,7 ./path
+*/1 * * * * ./path
